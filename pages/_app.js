a40e3b3800b748f6920c7982e9150b204cbda01f
Kaio Silveira <kaionpsa@gmail.com>
2023-01-31 09:32:08 -0300

    changes on package.json

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Metaversus</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="A complete modern website made with NextJS using Framer Motion and Tailwind CSS." />
+      <link rel="preconnect" href="https://stijndv.com" />
+      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
+
+export default MyApp;
